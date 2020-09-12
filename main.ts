3e77ff20a94c9e0758620f4b44e81cd698f52c54
Midesa19 <71153344+Midesa19@users.noreply.github.com>
2020-09-12 13:04:44 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -41,10 +41,24 @@ basic.forever(function () {
         }
     }
     while (index == 1) {
-        basic.showNumber(2)
-        basic.pause(250)
-        basic.clearScreen()
-        basic.pause(250)
+        if (input.buttonIsPressed(Button.AB) && !(isIn)) {
+            isIn = true
+            basic.pause(500)
+        } else {
+            if (input.buttonIsPressed(Button.AB) && isIn) {
+                isIn = false
+                basic.pause(500)
+            }
+        }
+        if (isIn) {
+            basic.showNumber(input.compassHeading())
+            basic.pause(500)
+        } else {
+            basic.showNumber(2)
+            basic.pause(250)
+            basic.clearScreen()
+            basic.pause(250)
+        }
     }
     while (index == 2) {
         basic.showNumber(3)

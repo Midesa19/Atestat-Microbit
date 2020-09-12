a3712470d0c84b33dad793af71f444783342bb03
Midesa19 <71153344+Midesa19@users.noreply.github.com>
2020-09-12 13:44:00 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -61,10 +61,43 @@ basic.forever(function () {
         }
     }
     while (index == 2) {
-        basic.showNumber(3)
-        basic.pause(250)
-        basic.clearScreen()
-        basic.pause(250)
+        if (input.buttonIsPressed(Button.AB) && !(isIn)) {
+            isIn = true
+            basic.pause(500)
+        } else {
+            if (input.buttonIsPressed(Button.AB) && isIn) {
+                isIn = false
+                basic.pause(500)
+            }
+        }
+        if (isIn) {
+            let a: number;
+            input.onGesture(Gesture.Shake, function () {
+                a=randint(0, 2)
+            })
+            basic.pause(500)
+            if (a == 0) {
+                basic.showIcon(IconNames.Scissors)
+            } else {
+                if (a == 1) {
+                    basic.showIcon(IconNames.SmallSquare)
+                } else {
+                    if(a==2)
+                        basic.showLeds(`
+                            # # # # #
+                            # # # # #
+                            # # # # #
+                            # # # # #
+                            # # # # #
+                            `)
+                }
+            }
+        } else {
+            basic.showNumber(3)
+            basic.pause(250)
+            basic.clearScreen()
+            basic.pause(250)
+        }
     }
     while (index == 3) {
         basic.showNumber(4)
